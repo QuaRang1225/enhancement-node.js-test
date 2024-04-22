0240087344a70fad2294912f0f675e7e9d03d7ab
QuaRang <dbduddnd1225@gmail.com>
2024-04-22 15:29:10 +0900

    DB 삭제 요청 구현
    - 가강의에는 .remove라고 나왔지만 원래는 .deleteOne 메서드를 사용해야함
    - products 목록 중 하나를 삭제하는 기능 추가

--- api/routes/products.js
+++ api/routes/products.js
@@ -66,4 +66,21 @@ router.get('/:productId',(req,res,next) => {
     
    
 })
+
+router.delete("/:productId",(req,res,next) => {
+    const id = req.params.productId
+    Product.deleteOne({ _id : id})
+    .exec()
+    .then(result => {
+        console.log(result)
+        res.status(200).json(result)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({
+            error:err
+        })
+    })
+})
+
 module.exports = router
