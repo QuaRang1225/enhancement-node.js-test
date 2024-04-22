e84cb9c86ff98dc808e6837ff8dd33bbdc353a66
QuaRang <dbduddnd1225@gmail.com>
2024-04-22 21:17:00 +0900

    요청 필드 타입 변경해도 요청 가도록 수정
    - 기존 price 필드는 숫자만 해당하는 필드였지만 문자열로 보내더라도 알앟서 정수형으로 바꿔서 저장되도록 구현
    - name 필드도 마찬가지로 위와 같이 수정

--- api/models/products.js
+++ api/models/products.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose')
 
 const prodcutSchema = mongoose.Schema({
     _id : mongoose.Schema.Types.ObjectId,
-    name: String,
-    price:Number
+    name: {type:String,required:true},
+    price:{type: Number,required:true}
 })
 
 module.exports = mongoose.model('Product',prodcutSchema)
