5d21218b8771f6ba54de9eaa6db01ced3e451820
QuaRang <dbduddnd1225@gmail.com>
2024-04-23 14:35:41 +0900

    관계형 데이터베이스 구축

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -3,31 +3,73 @@ const mongoose = require('mongoose')
 const router = express.Router()
 
 const Order = require('../models/order')
-
+const Product = require('../models/products')
+  
 router.get('/',(req,res,next)=>{
-    res.status(200).json({
-        message : 'Orders were fetched'
+    Order.find()
+    .select('product quantity _id')
+    .exec()
+    .then(docs =>{
+        res.status(200).json({
+            count: docs.length,
+            order:docs.map(doc=>{
+                return{
+                    _id:doc._id,
+                    prodcut : doc.product,
+                    quantity:doc.quantity,
+                    request:{
+                        type:'GET',
+                        url: 'http://localhost:3000/order/' + doc._id
+                    }
+                }
+            }),
+        })
+    })
+    .catch(err =>{
+        console.log(err)
+        res.status(500).json({
+            error:err
+        })
     })
 })
 
 router.post('/',(req,res,next)=>{
-    const order = new Order({
-        _id: new mongoose.Types.ObjectId(),
-        quantity:req.body.quantity,
-        product: req.body.productId
-    })
-    order
-        .save()
-        .then(result=> {
-            console.log(result)
-            res.status(201).json(result)
-        })
-        .catch(err =>{
-            console.log(err)
-            res.status(500).json({
-                error:err
+    Product.findById(req.body.productId)
+    .then(product => {
+        if (!product){
+            return res.status(404).json({
+                message : "Product not found"
             })
+        }
+        const order = new Order({
+            _id: new mongoose.Types.ObjectId(),
+            quantity:req.body.quantity,
+            product: req.body.productId
         })
+        return order.save()
+    })
+    .then(result=> {
+        console.log(result)
+        res.status(201).json({
+            message : 'Order stored',
+            createdOrder:{
+                _id:result._id,
+                prodcut : result.product,
+                quantity:result.quantity
+            },
+            request:{
+                type:'POST',
+                url: 'http://localhost:3000/order/' + result._id
+            }
+        })
+    })
+    .catch(err =>{
+        console.log(err)
+        res.status(500).json({
+            error:err
+        })
+    })
+  
 })
 
 router.get('/:orderId',(req,res,next)=>{
