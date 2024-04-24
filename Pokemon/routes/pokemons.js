fda86c5bd7dfdf8dbb8d4bba2559b3b3f4883fae
QuaRang <dbduddnd1225@gmail.com>
2024-04-24 16:57:44 +0900

    특정 포켓몬 정보 요청
    - 포켓몬 ID를 endPoint로 붙여 해당 포켓몬에 대한 정보만 반환하도록 구현

--- Pokemon/routes/pokemons.js
+++ Pokemon/routes/pokemons.js
@@ -6,6 +6,32 @@ const Pokemon = require('../models/pokemon')
 const Varieites = require('../models/vaieites')
 
 
+router.get('/:pokemonId',(req,res,next) => {
+    const id = req.params.pokemonId
+    Pokemon.findById(id)
+    .exec()
+    .then(doc => {
+        if (doc){
+            res.status(200).json({
+                status : 200,
+                data:responseValue(doc),
+                message : "정상적으로 포켓몬 정보를 조회했습니다."
+            })
+        }else{
+            res.status(404).json({
+                status : 404,
+                message : "포켓몬 ID가 존재하지 않습니다."
+            })
+        }
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({
+            status : 500,
+            message : "포켓몬 정보 요청이 실패했습니다."
+        })
+    })
+})
 router.get('/', (req, res, next) => {
     const page = parseInt(req.query.page) || 1; // 페이지 번호, 기본값은 1
     const perPage = 20; // 페이지당 아이템 수
@@ -26,22 +52,7 @@ router.get('/', (req, res, next) => {
                         current_page: page, // 현재 페이지 번호
                         per_page: perPage, // 페이지당 아이템 수
                         pokemon: docs.map(doc => {
-                            return {
-                                _id: doc._id,
-                                capture_rate: doc.capture_rate,
-                                dex_num: doc.dex_num,
-                                dex_region: doc.dex_region,
-                                egg_group: doc.egg_group,
-                                evolution_tree: doc.evolution_tree,
-                                forms_switchable: doc.forms_switchable,
-                                gender_rate: doc.gender_rate,
-                                genra: doc.genra,
-                                hatch_counter: doc.hatch_counter,
-                                name: doc.name,
-                                text_entries_text: doc.text_entries_text,
-                                text_entries_version: doc.text_entries_version,
-                                varieites: doc.varieites
-                            }
+                            return responseValue(doc)
                         })
                     };
                     res.status(200).json(response);
@@ -57,22 +68,7 @@ router.get('/', (req, res, next) => {
 
 
 router.post('/',(req,res,next) =>{
-    const pokemon = new Pokemon({
-        _id : req.body._id,
-        capture_rate : req.body.capture_rate,
-        dex_num : req.body.dex_num,
-        dex_region : req.body.dex_region,
-        egg_group : req.body.egg_group,
-        evolution_tree : req.body.evolution_tree,
-        forms_switchable : req.body.forms_switchable,
-        gender_rate : req.body.gender_rate,
-        genra : req.body.genra,
-        hatch_counter : req.body.hatch_counter,
-        name : req.body.name,
-        text_entries_text : req.body.text_entries_text ,
-        text_entries_version : req.body.text_entries_version,
-        varieites : req.body.varieites
-    })
+    const pokemon = new Pokemon(requestValue(req))
     pokemon
     .save()
     .then(result => {
@@ -84,12 +80,54 @@ router.post('/',(req,res,next) =>{
     .catch(err => {
         if (err.errorResponse.code == 11000){
             console.log(err)
+            res.status(400).json({
+                status : 400,
+                message : "중복된 데이터 입니다"
+            })
+        }else{
             res.status(500).json({
                 status : 500,
-                message : "중복된 데이터 입니다"
+                message : "데이터 저장이 실패했습니다."
             })
         }
     })
 })
+
+function responseValue(doc){
+    return {
+        _id: doc._id,
+        capture_rate: doc.capture_rate,
+        dex_num: doc.dex_num,
+        dex_region: doc.dex_region,
+        egg_group: doc.egg_group,
+        evolution_tree: doc.evolution_tree,
+        forms_switchable: doc.forms_switchable,
+        gender_rate: doc.gender_rate,
+        genra: doc.genra,
+        hatch_counter: doc.hatch_counter,
+        name: doc.name,
+        text_entries_text: doc.text_entries_text,
+        text_entries_version: doc.text_entries_version,
+        varieites: doc.varieites
+    }
+}
+function requestValue(req){
+    return {
+        _id: req.body._id,
+        capture_rate: req.body.capture_rate,
+        dex_num: req.body.dex_num,
+        dex_region: req.body.dex_region,
+        egg_group: req.body.egg_group,
+        evolution_tree: req.body.evolution_tree,
+        forms_switchable: req.body.forms_switchable,
+        gender_rate: req.body.gender_rate,
+        genra: req.body.genra,
+        hatch_counter: req.body.hatch_counter,
+        name: req.body.name,
+        text_entries_text: req.body.text_entries_text,
+        text_entries_version: req.body.text_entries_version,
+        varieites: req.body.varieites
+    }
+}
 
 module.exports = router
