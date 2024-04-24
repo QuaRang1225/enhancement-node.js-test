7406678e710594cd6905619838317f3bdc6b52b5
QuaRang <dbduddnd1225@gmail.com>
2024-04-24 18:52:10 +0900

    포켓몬 정보 수정
    - patch 요청으로 특정 포켓몬 정보 수정할 수 있도록 구현

--- Pokemon/routes/pokemons.js
+++ Pokemon/routes/pokemons.js
@@ -80,7 +80,7 @@ router.post('/',(req,res,next) =>{
     .catch(err => {
         if (err.errorResponse.code == 11000){
             console.log(err)
-            res.status(400).json({
+            res.status(409).json({
                 status : 400,
                 message : "중복된 데이터 입니다"
             })
@@ -92,7 +92,33 @@ router.post('/',(req,res,next) =>{
         }
     })
 })
-
+router.patch("/:pokemonId",(req,res,next) => {
+    const id = req.params.pokemonId
+    const pokemon = requestValue(req)
+    Pokemon.updateOne({ _id : id },pokemon)
+    .exec()
+    .then(result => {
+        if (result){
+            res.status(200).json({
+                status : 200,
+                data:pokemon,
+                message : "정상적으로 포켓몬 정보를 수정했습니다."
+            })
+        }else{
+            res.status(404).json({
+                status : 404,
+                message : "포켓몬 ID가 존재하지 않습니다."
+            })
+        }
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({
+            status : 500,
+            message : "포켓몬 정보 요청이 실패했습니다."
+        })
+    }) 
+})
 function responseValue(doc){
     return {
         _id: doc._id,
