975422058975f4c0e3cc91233bbbe5dd627259fe
QuaRang <dbduddnd1225@gmail.com>
2024-04-24 19:41:43 +0900

    포켓몬 삭제요청
    - 포캣몬 삭제요청 API 구현
    - 포켓몬 수정 요청 시 이미 삭제된 요청에도 수정 요청이 정상적으로 되는 문제 해결

--- Pokemon/routes/pokemons.js
+++ Pokemon/routes/pokemons.js
@@ -33,8 +33,8 @@ router.get('/:pokemonId',(req,res,next) => {
     })
 })
 router.get('/', (req, res, next) => {
-    const page = parseInt(req.query.page) || 1; // 페이지 번호, 기본값은 1
-    const perPage = 20; // 페이지당 아이템 수
+    const page = parseInt(req.query.page) || 1 // 페이지 번호, 기본값은 1
+    const perPage = 20 // 페이지당 아이템 수
 
     Pokemon
         .countDocuments() // 전체 문서 수를 가져옴
@@ -45,7 +45,7 @@ router.get('/', (req, res, next) => {
                 .limit(perPage) // 한 페이지에 반환할 아이템 수 제한
                 .exec()
                 .then(docs => {
-                    const totalPages = Math.ceil(totalCount / perPage); // 전체 페이지 수 계산
+                    const totalPages = Math.ceil(totalCount / perPage) // 전체 페이지 수 계산
                     const response = {
                         total_count: totalCount, // 전체 아이템 수
                         total_pages: totalPages, // 전체 페이지 수
@@ -55,16 +55,20 @@ router.get('/', (req, res, next) => {
                             return responseValue(doc)
                         })
                     };
-                    res.status(200).json(response);
+                    res.status(200).json(response)
                 })
                 .catch(err => {
-                    res.status(500).json({ error: err });
-                });
+                    res.status(500).json({ error: err })
+                })
         })
         .catch(err => {
-            res.status(500).json({ error: err });
-        });
-});
+            res.status(500).json({
+                status : 500,
+                data : {},
+                message : "데이터를 불러오는데 실패했습니다."
+            })
+        })
+})
 
 
 router.post('/',(req,res,next) =>{
@@ -74,6 +78,7 @@ router.post('/',(req,res,next) =>{
     .then(result => {
         res.status(201).json({
             status : 201,
+            data : {},
             message : result.name + "의 데이터 저장 완료"
         })
     })
@@ -82,11 +87,13 @@ router.post('/',(req,res,next) =>{
             console.log(err)
             res.status(409).json({
                 status : 400,
+                data : {},
                 message : "중복된 데이터 입니다"
             })
         }else{
             res.status(500).json({
                 status : 500,
+                data : {},
                 message : "데이터 저장이 실패했습니다."
             })
         }
@@ -98,7 +105,7 @@ router.patch("/:pokemonId",(req,res,next) => {
     Pokemon.updateOne({ _id : id },pokemon)
     .exec()
     .then(result => {
-        if (result){
+        if (result.matchedCount > 0){
             res.status(200).json({
                 status : 200,
                 data:pokemon,
@@ -107,15 +114,44 @@ router.patch("/:pokemonId",(req,res,next) => {
         }else{
             res.status(404).json({
                 status : 404,
+                data : {},
                 message : "포켓몬 ID가 존재하지 않습니다."
             })
         }
     })
     .catch(err => {
-        console.log(err)
         res.status(500).json({
             status : 500,
-            message : "포켓몬 정보 요청이 실패했습니다."
+            data : {},
+            message : "포켓몬 정보 수정이 실패했습니다."
+        })
+    }) 
+})
+
+router.delete("/:pokemonId",(req,res,next) => {
+    const id = req.params.pokemonId
+    Pokemon.deleteOne({ _id : id})
+    .exec()
+    .then(result => {
+        if (result){
+            res.status(200).json({
+                status : 200,
+                data:{},
+                message : "정상적으로 포켓몬 정보를 삭제했습니다."
+            })
+        }else{
+            res.status(404).json({
+                status : 404,
+                data : {},
+                message : "포켓몬 ID가 존재하지 않습니다."
+            })
+        }
+    })
+    .catch(err => {
+        res.status(500).json({
+            status : 500,
+            data : {},
+            message : "포켓몬 삭제 요청이 실패했습니다."
         })
     }) 
 })
