dabc4087472a983286d33276dcf61243ce223b2a
QuaRang <dbduddnd1225@gmail.com>
2024-04-29 20:31:41 +0900

    포켓몬 진화트리 관련
    - 포켓몬 진화트리 코드 수정을 위한 PATCH요청 구현
    - 포켓몬 진화트리 코드 삭제를 위한 DELETE요청 구현

--- Pokemon/routes/evolutionTree.js
+++ Pokemon/routes/evolutionTree.js
@@ -60,6 +60,63 @@ router.get('/:evolId',(req,res,next) => {
     })
 })
 
+router.patch("/:evolId",(req,res,next) => {
+    const id = req.params.evolId
+    const pokemon = requestValue(req)
+    EvolTree.updateOne({ _id : id },pokemon)
+    .exec()
+    .then(result => {
+        if (result.matchedCount > 0){
+            res.status(200).json({
+                status : 200,
+                data:pokemon,
+                message : "정상적으로 포켓몬 진화트리 정보를 수정했습니다."
+            })
+        }else{
+            res.status(404).json({
+                status : 404,
+                data : {},
+                message : "포켓몬 ID가 존재하지 않습니다."
+            })
+        }
+    })
+    .catch(err => {
+        res.status(500).json({
+            status : 500,
+            data : {},
+            message : "포켓몬 진화트리 정보 수정이 실패했습니다."
+        })
+    }) 
+})
+
+router.delete("/:evolId",(req,res,next) => {
+    const id = req.params.evolId
+    EvolTree.deleteOne({ _id : id})
+    .exec()
+    .then(result => {
+        if (result){
+            res.status(200).json({
+                status : 200,
+                data:{},
+                message : "정상적으로 포켓몬 진화트리 정보를 삭제했습니다."
+            })
+        }else{
+            res.status(404).json({
+                status : 404,
+                data : {},
+                message : "포켓몬 ID가 존재하지 않습니다."
+            })
+        }
+    })
+    .catch(err => {
+        res.status(500).json({
+            status : 500,
+            data : {},
+            message : "포켓몬 진화트리 삭제 요청이 실패했습니다."
+        })
+    }) 
+})
+
 function requestValue(req){
     return {
         _id : req.body._id,
